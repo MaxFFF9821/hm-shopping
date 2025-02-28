3c0f4e5f3e2d8065018d0b358d5c6efbd780c724
maxfeng9821 <jsmax.f@hotmail.com>
2025-02-28 11:21:39 -0500

    add home layout with navigation, search, and product display components

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
-import { Button, Rate, Tabbar, TabbarItem, NavBar, Toast } from 'vant'
+import { Button, Rate, Tabbar, TabbarItem, NavBar, Toast, Swipe, SwipeItem, Grid, Search, GridItem } from 'vant'
 Vue.use(Button)
 Vue.use(Rate)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(NavBar)
 Vue.use(Toast)
+Vue.use(Swipe)
+Vue.use(Grid)
+Vue.use(Search)
+Vue.use(GridItem)
+Vue.use(SwipeItem)
